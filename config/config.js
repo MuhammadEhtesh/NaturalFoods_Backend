93ca4a90aef0c9258b46f16de992c63777df6095
Muhammad Ehtesham <Shani@Muhammads-MacBook-Air.local>
2020-06-28 22:01:32 +0500

    JWT implementation

--- config/config.js
+++ config/config.js
@@ -3,4 +3,5 @@ module.exports = {
     ? process.env.MONGO_URL
     : "mongodb+srv://shani:aqsPa86HMrQfpvxY@measstackdbcluster-wn3iz.mongodb.net/NaturalFoods?retryWrites=true&w=majority",
   PORT: process.env.PORT ? process.env.PORT : 8080,
+  JsonSecretKey: "sgdfhgjkgdfuawiygefkjahsdfuaoser",
 };
--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,5 +1,7 @@
-const Register = require("../models/register");
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
+const Register = require("../models/register");
 
 const router = express.Router();
 
@@ -16,13 +18,58 @@ router.post("/register", (req, res) => {
 router.post("/login", (req, res) => {
   Register.find(
     { username: req.body.username, password: req.body.password },
-    (err, doc) => {
+    (err, user) => {
       if (err) {
         return res.status(404).send(err);
       }
-      res.status(200).json(doc);
+      jwt.sign(
+        { user },
+        config.JsonSecretKey,
+        { expiresIn: "30m" },
+        (err, token) => {
+          if (err) {
+            res.status(404).send(err);
+          }
+          res.status(200).json({
+            token: token,
+            user: user,
+          });
+        }
+      );
     }
   );
 });
+
+// basic implementation for jwt verification
+router.get("/registers", verifyToken, (req, res) => {
+  jwt.verify(req.token, config.JsonSecretKey, (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      Register.find((err, registers) => {
+        if (err) {
+          return res.send(err);
+        }
+        res.json({
+          authData,
+          registers,
+        });
+      });
+    }
+  });
+});
+
+// jwt token verification
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 
 module.exports = router;
