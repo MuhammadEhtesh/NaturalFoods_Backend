fa99cdc34bed5ffd6e507677e163478efaa544c0
Muhammad Ehtesham <Shani@192.168.10.5>
2020-07-15 13:06:19 +0500

    mobile property removed

--- models/register.js
+++ models/register.js
@@ -5,7 +5,6 @@ const registerSchema = new mongoose.Schema(
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
     cnic: { type: String, required: true },
-    mobile: { type: String, required: true },
     email: { type: String },
     password: { type: String, required: true },
     confirmpassword: { type: String, required: true },
